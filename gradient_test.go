5808c777fd8b1c205ac944bc62790b75cf10e30f
Evan Beeton <3587393+ebeeton@users.noreply.github.com>
2023-03-03 17:34:56 -0500

    Write a better gradient test.

--- gradient_test.go
+++ gradient_test.go
@@ -4,31 +4,37 @@ package gradient
 
 import (
 	"image/color"
-	"math"
 	"testing"
 )
 
 func TestGetGradient(t *testing.T) {
-	black := color.RGBA{R: 0, G: 0, B: 0, A: math.MaxUint8}
-	grey := color.RGBA{R: math.MaxUint8 >> 1, G: math.MaxUint8 >> 1, B: math.MaxUint8 >> 1, A: math.MaxUint8}
-	white := color.RGBA{R: math.MaxUint8, G: math.MaxUint8, B: math.MaxUint8, A: math.MaxUint8}
-
 	stops := []Stop{
-		{Color: black, Position: 0.0},
-		{Color: white, Position: 1.0},
+		{Color: color.RGBA{R: 0, G: 255, B: 0, A: 255}, Position: 0.0},
+		{Color: color.RGBA{R: 255, G: 0, B: 255, A: 0}, Position: 1.0},
 	}
-
-	wantStops := 3
+	wantColors := []string{
+		"#00ff00ff",
+		"#1fdf1fdf",
+		"#3fbf3fbf",
+		"#5f9f5f9f",
+		"#7f7f7f7f",
+		"#9f5f9f5f",
+		"#bf3fbf3f",
+		"#df1fdf1f",
+		"#ff00ff00",
+	}
+	wantStops := 9
 	g := GetGradient(stops, wantStops)
 
 	if len(g) != wantStops {
 		t.Errorf("Got stops %d, want %d.", len(g), wantStops)
-	} else if g[0] != black {
-		t.Errorf("Got black %v, want %v.", g[0], black)
-	} else if g[1] != grey {
-		t.Errorf("Got grey %v, want %v.", g[1], grey)
-	} else if g[2] != white {
-		t.Errorf("Got white %v, want %v.", g[2], white)
+	}
+
+	for i, c := range g {
+		var gotHex = ColorToHex(c, true)
+		if gotHex != wantColors[i] {
+			t.Errorf("Got %s, want %s.", gotHex, wantColors[i])
+		}
 	}
 }
 
